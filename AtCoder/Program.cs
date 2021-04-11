7bbec00f04891729989e7760406aa14f4b02d999
sakastudio <sakastudio100@gmail.com>
2021-04-11 22:45:21 +0900

    最初のコミット AtCoder Beginner Contest 198 C問題

---
+++ AtCoder/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AtCoder
+{
+    /*
+// 文字列の入力
+string s = Console.ReadLine();
+
+// 整数の入力
+long n = long.Parse(Console.ReadLine());
+
+// 少数の入力
+double n = double.Parse(Console.ReadLine());
+
+// 文字列配列の入力
+string[] inputStrArray = Console.ReadLine().Split(' ');
+
+// 整数配列の入力
+long[] inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+     */
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            string s3 = Console.ReadLine();
+            
+            var kuriagari = new int[s1.Length];
+            var ans = new int[s3.Length];
+            
+            for (int i = 0; i < s1.Length; i++)
+            {
+                for (int j = 0; j < s2.Length; j++)
+                {
+                    
+                }
+            }
+        }
+    }
+}
